7f0588c3c691c3e475104993b07cd331d53f0240
dydtjd0245 <dydtjd0245@gmail.com>
2019-10-13 22:09:37 +0900

    Updates

---
+++ precache-manifest.0bfc1f88988e7e2fa2a08d37c92ace1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bffbb2daec4c4597c15b144a8c065d26",
+    "url": "/react/first_react/index.html"
+  },
+  {
+    "revision": "f9a0c2216a3e3caadaff",
+    "url": "/react/first_react/static/css/main.4261daff.chunk.css"
+  },
+  {
+    "revision": "a844320599ac8210a8ce",
+    "url": "/react/first_react/static/js/2.22cb3b4d.chunk.js"
+  },
+  {
+    "revision": "f9a0c2216a3e3caadaff",
+    "url": "/react/first_react/static/js/main.c9b5418e.chunk.js"
+  },
+  {
+    "revision": "e0d0b472ce4b8089f3e7",
+    "url": "/react/first_react/static/js/runtime-main.2a34970f.js"
+  }
+]);
